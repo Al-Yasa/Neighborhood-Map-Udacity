b3a5670b47a514e98f57bc0dc7bbe1df346963ea
Belhenniche Yassaa <Yagon66@gmail.com>
2019-06-05 18:18:21 +0100

    Updates

---
+++ precache-manifest.5ec18ce3de12126f7cbfad7c1f4f0b67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "191ccfae4cad6c85499a",
+    "url": "/Neighborhood-Map-Udacity/static/js/runtime~main.5eef50e2.js"
+  },
+  {
+    "revision": "697f1f8a46ec1a804ad5",
+    "url": "/Neighborhood-Map-Udacity/static/js/main.a5919baf.chunk.js"
+  },
+  {
+    "revision": "33741c69ed7563e9f551",
+    "url": "/Neighborhood-Map-Udacity/static/js/2.78d2e4de.chunk.js"
+  },
+  {
+    "revision": "697f1f8a46ec1a804ad5",
+    "url": "/Neighborhood-Map-Udacity/static/css/main.16356498.chunk.css"
+  },
+  {
+    "revision": "9c971dfcfa5b9c81ebe071d33124a352",
+    "url": "/Neighborhood-Map-Udacity/index.html"
+  }
+];
